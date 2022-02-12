f445662881ba19dba67e72b3ca0dac654042f7ad
Jacek Tomaszewski <jacek.tomek@gmail.com>
2022-02-12 18:46:54 +0100

    Add ko-fi button and better description.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      doi2better
+      doi2better - easy transformation of DOI to many bibliography formats
     </v-app-bar>
 
     <v-main>
       <Home/>
     </v-main>
+    <v-footer padless>
+      <v-col cols="12" class="text-center">
+        <a id="kofi" href="https://ko-fi.com/zlorf" target="_blank" style="">
+          <img src="https://cdn.ko-fi.com/cdn/kofi2.png?v=3" alt="Buy Me a Coffee / Donate">
+        </a>
+      </v-col>
+    </v-footer>
   </v-app>
 </template>
 
@@ -22,3 +29,13 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style lang="less">
+#kofi {
+  img {
+    border: 0;
+    height: 36px;
+    vertical-align: middle;
+  }
+}
+</style>
